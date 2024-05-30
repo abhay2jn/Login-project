96d817ca2b1e04889212cf0c742d680caf52c511
abhay <abhayforwork02@gmail.com>
2024-05-30 23:26:53 +0530

    router

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const about = () => {
+function about() {
   return (
     <div>about</div>
   )
 }
 
-export default about
+export default about;
--- app/page.js
+++ app/page.js
@@ -1,8 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 
 function page() {
+  const router = useRouter();
+
+  const any = (page) => {
+    router.push(page);
+  }
+  
   return (
-    <div>Page</div>
+    <section>
+      <h1>Routing</h1>
+      <button className="border px-2 py-4" onClick={() => any("about")}>Go to about page</button>
+    </section>
   )
 }
 
 export default page;
+// onClick={() => router.push("about")} first method
